d1cecd65c7dc2a2b9cfbdaacd15d7f4766bde78d
Alex P <alexperis95@gmail.com>
2024-12-11 11:07:15 +0100

    fix: :adhesive_bandage: Delete Inertia

--- routes/web.php
+++ routes/web.php
@@ -1,21 +1,17 @@
 <?php
 
 use App\Http\Controllers\ProfileController;
-use Illuminate\Foundation\Application;
 use Illuminate\Support\Facades\Route;
-use Inertia\Inertia;
+use App\Livewire\Home;
+use App\Livewire\CreateReservation;
 
-Route::get('/', function () {
-    return Inertia::render('Home', [
-        'canLogin' => Route::has('login'),
-        'canRegister' => Route::has('register'),
-        'laravelVersion' => Application::VERSION,
-        'phpVersion' => PHP_VERSION,
-    ]);
-});
+
+Route::get('/', Home::class)->name('home');
+Route::get('/reservar', CreateReservation::class)->name('reservar');
+
 
 Route::get('/dashboard', function () {
-    return Inertia::render('Dashboard');
+    return view('dashboard');
 })->middleware(['auth', 'verified'])->name('dashboard');
 
 Route::middleware('auth')->group(function () {
