82c33d2f02ff2a611241da7c1bb00f6b6e5222a0
Alex P <alexperis95@gmail.com>
2024-12-17 13:54:31 +0100

    feat: :sparkles: Update profile and change password

--- resources/views/profile/partials/update-profile-information-form.blade.php
+++ resources/views/profile/partials/update-profile-information-form.blade.php
@@ -9,46 +9,39 @@
         </p>
     </header>
 
-    <form id="send-verification" method="post" action="{{ route('verification.send') }}">
+    <form method="post" action="{{ route('profile.update') }}" class="mt-6 space-y-6" x-data="formHandler()" id="profile-form">
         @csrf
-    </form>
-
-    <form method="post" action="{{ route('profile.update') }}" class="mt-6 space-y-6">
-        @csrf
+        
         @method('patch')
 
         <div>
-            <x-input-label for="name" :value="__('Name')" />
-            <x-text-input id="name" name="name" type="text" class="mt-1 block w-full" :value="old('name', $user->name)" required autofocus autocomplete="name" />
-            <x-input-error class="mt-2" :messages="$errors->get('name')" />
+            <x-input-label for="nombre" :value="__('Nombre')" />
+            <x-text-input id="nombre" name="nombre" type="text" class="mt-1 block w-full" :value="old('nombre', $user->nombre)" required autofocus autocomplete="nombre" />
+            <x-input-error class="mt-2" :messages="$errors->get('nombre')" />
         </div>
 
         <div>
-            <x-input-label for="email" :value="__('Email')" />
-            <x-text-input id="email" name="email" type="email" class="mt-1 block w-full" :value="old('email', $user->email)" required autocomplete="username" />
+            <x-input-label for="apellido" :value="__('Apellido')" />
+            <x-text-input id="apellido" name="apellido" type="text" class="mt-1 block w-full" :value="old('apellido', $user->apellido)" required autofocus autocomplete="apellido" />
+            <x-input-error class="mt-2" :messages="$errors->get('apellido')" />
+        </div>
+
+        <div>
+            <x-input-label for="number_phone" :value="__('Número de teléfono')" />
+            <x-text-input id="number_phone" name="number_phone" type="text" class="mt-1 block w-full" :value="old('number_phone', $user->number_phone)" required autofocus autocomplete="number_phone" />
+            <x-input-error class="mt-2" :messages="$errors->get('number_phone')" />
+        </div>
+
+        <div>
+            <x-input-label for="email" :value="__('Correo electrónico')" />
+            <x-text-input id="email" name="email" type="email" class="mt-1 block w-full" :value="old('email', $user->email)" required autofocus autocomplete="email" />
             <x-input-error class="mt-2" :messages="$errors->get('email')" />
-
-            @if ($user instanceof \Illuminate\Contracts\Auth\MustVerifyEmail && ! $user->hasVerifiedEmail())
-                <div>
-                    <p class="text-sm mt-2 text-gray-800">
-                        {{ __('Your email address is unverified.') }}
-
-                        <button form="send-verification" class="underline text-sm text-gray-600 hover:text-gray-900 rounded-md focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500">
-                            {{ __('Click here to re-send the verification email.') }}
-                        </button>
-                    </p>
-
-                    @if (session('status') === 'verification-link-sent')
-                        <p class="mt-2 font-medium text-sm text-green-600">
-                            {{ __('A new verification link has been sent to your email address.') }}
-                        </p>
-                    @endif
-                </div>
-            @endif
         </div>
 
         <div class="flex items-center gap-4">
-            <x-primary-button>{{ __('Save') }}</x-primary-button>
+            <x-primary-button x-bind:disabled="!isDirty" x-bind:class="isDirty ? 'bg-blue-500 cursor-pointer' : 'bg-gray-400 cursor-not-allowed'" id="save-button">
+
+            {{ __('Save') }}</x-primary-button>
 
             @if (session('status') === 'profile-updated')
                 <p
