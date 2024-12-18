be8f017a98b872e7efa427814bfc06adf6f34a89
Alex P <alexperis95@gmail.com>
2024-12-18 11:52:40 +0100

    feat: :sparkles: create recover pass and 404 pages

--- routes/web.php
+++ routes/web.php
@@ -31,4 +31,8 @@ Route::middleware('auth')->group(function () {
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
 });
 
+Route::fallback(function () {
+    return view('errors.404');
+});
+
 require __DIR__.'/auth.php';
