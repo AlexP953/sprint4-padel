029d6ef63c92c3b0341cc58979064bd4739595b9
Alex P <alexperis95@gmail.com>
2024-12-19 21:34:31 +0100

    feat: readme and script

--- database/seeders/ReservationSeeder.php
+++ database/seeders/ReservationSeeder.php
@@ -11,11 +11,13 @@ class ReservationSeeder extends Seeder
     public function run()
     {
         $faker = Faker::create();
-
+    
         for ($i = 0; $i < 10; $i++) {
             $fecha_inicio = $faker->dateTimeBetween('now', '+1 month');
+            $fecha_inicio->setTime($fecha_inicio->format('H'), 0);
+    
             $fecha_final = (clone $fecha_inicio)->modify('+1 hour');
-
+    
             DB::table('reservations')->insert([
                 'id_user' => $faker->numberBetween(1, 10),
                 'fecha_inicio' => $fecha_inicio,
@@ -23,18 +25,21 @@ class ReservationSeeder extends Seeder
                 'id_pista' => $faker->numberBetween(1, 8),
             ]);
         }
-
+    
         DB::table('reservations')->insert([
-            'id_user' => 11,
-            'fecha_inicio' => $faker->dateTimeBetween('now', '+1 month'),
-            'fecha_final' => (clone $fecha_inicio)->modify('+1 hour'),
-            'id_pista' => $faker->numberBetween(1, 8),
-        ],
-        [
-            'id_user' => 11,
-            'fecha_inicio' => $faker->dateTimeBetween('now', '+1 month'),
-            'fecha_final' => (clone $fecha_inicio)->modify('+1 hour'),
-            'id_pista' => $faker->numberBetween(1, 8),
+            [
+                'id_user' => 11,
+                'fecha_inicio' => $faker->dateTimeBetween('now', '+1 month')->setTime(0, 0),
+                'fecha_final' => (clone $fecha_inicio)->modify('+1 hour'),
+                'id_pista' => $faker->numberBetween(1, 8),
+            ],
+            [
+                'id_user' => 11,
+                'fecha_inicio' => $faker->dateTimeBetween('now', '+1 month')->setTime(0, 0),
+                'fecha_final' => (clone $fecha_inicio)->modify('+1 hour'),
+                'id_pista' => $faker->numberBetween(1, 8),
+            ]
         ]);
     }
+    
 }
